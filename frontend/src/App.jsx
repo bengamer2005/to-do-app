93fff3ed96c340d494da5f077a8541ed79240891
bengamer2005 <garciabenitoleonel@gmail.com>
2025-04-17 01:42:49 -0600

    Apartado creado para el historial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import TaskPage from "./pages/taskPage"
+import HistorialPage from "./pages/historialPage"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TaskPage/>} />
+        <Route path="/historial-tasks" element={<HistorialPage/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/historialPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import Header from "../components/header"
+import TaskCard from "../components/taskCard"
+import useTasksGet from "../hooks/useTasksGet"
+import "../styles/global.css"
+
+const HistorialPage = () => {
+    const [taskTitle, setTaskTitle] = useState("")
+    const [taskDescription, setTaskDescription] = useState("")
+    
+    const { tasks, fetchTasks } = useTasksGet()
+
+    const putValue = (name, value) => {
+        if(name === "taskTitle") {
+            setTaskTitle(value)
+        } else if(name === "taskDescription") {
+            setTaskDescription(value)
+        }
+    }
+
+    return (
+        <>
+            <Header/>
+            <div className="space"></div>
+            <div className="cards">
+                {tasks.map((newTask, i) => (
+                    <TaskCard
+                        key={i}
+                        title={newTask.taskTitle}
+                        status={newTask.status}
+                        description={newTask.taskDescription}
+                    />
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default HistorialPage
