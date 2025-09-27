f891e1435b3dda38fc89c9537507b926f5fe2a30
bengamer2005 <garciabenitoleonel@gmail.com>
2025-09-26 20:38:09 -0600

    variables de entorno para el backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: "./.env" })
 // se usa cors para poder hacer las solicitudes desde mi frontend
 const cors = require("cors")
 app.use(cors({
-    origin: "http://localhost:5172"
+    origin: process.env.FRONTRND_URL
 }))
 
 // creamos los endpoints
